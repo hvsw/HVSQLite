d58fa0a8f3c496f339c39b247156760ae7d0542f
Henrique Valcanaia <henriqueindalencio@gmail.com>
2018-05-15 22:10:16 -0300

    Initial Commit

---
+++ HVSQLite/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HVSQLite
+//
+//  Created by Henrique Valcanaia on 15/05/18.
+//  Copyright © 2018 Henrique Valcanaia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
