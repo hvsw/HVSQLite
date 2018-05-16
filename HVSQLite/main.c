bdaa55a3beda3d12009b7b1957ab10f50203ab6b
Henrique Valcanaia <henriqueindalencio@gmail.com>
2018-05-16 00:30:41 -0300

    Added string length validation to email and username

--- HVSQLite/main.c
+++ HVSQLite/main.c
@@ -57,7 +57,8 @@ typedef enum MetaCommandResult_t MetaCommandResult;
 enum PrepareResult_t {
     PREPARE_SUCCESS,
     PREPARE_SYNTAX_ERROR,
-    PREPARE_UNRECOGNIZED_STATEMENT
+    PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_STRING_TOO_LONG
 };
 typedef enum PrepareResult_t PrepareResult;
 
@@ -138,18 +139,41 @@ void* row_slot(Table* table, uint32_t row_num) {
     return  page+byte_offset;
 }
 
+
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+    statement->type = STATEMENT_INSERT;
+    
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+    
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+    
+    int id = atoi(id_string);
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+    
+    return PREPARE_SUCCESS;
+}
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
     /*
      * `strncmp` is used for insert because
      * we have more data after it like: `insert a b c`
      */
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     
     if (strcmp(input_buffer->buffer, "select") == 0) {
@@ -219,6 +243,10 @@ int main(int argc, const char * argv[]) {
         switch (prepare_statement(input_buffer, &statement)) {
             case PREPARE_SUCCESS:
                 break;
+                
+            case PREPARE_STRING_TOO_LONG:
+                printf("String is too long.\n");
+                continue;
                 
             case PREPARE_SYNTAX_ERROR:
                 printf("Syntax error. Could not parse statement.\n");
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -54,4 +54,20 @@ describe 'database' do
           "db > ",
         ])
     end
+
+    it 'prints error message if strings are too long' do
+        long_username = "a"*33
+        long_email = "a"*256
+        script = [
+            "insert 1 #{long_username} #{long_email}",
+            "select",
+            ".exit",
+        ]
+        result = run_script(script)
+        expect(result).to match_array([
+          "db > String is too long.",
+          "db > Executed.",
+        "db > ",
+        ])
+    end
 end
